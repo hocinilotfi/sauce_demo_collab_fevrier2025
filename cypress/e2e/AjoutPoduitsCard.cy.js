f3c13d8ac4ba44aa672ba03d75f111cc151e9b36
AchourSimoud <achoursimoud@gmail.com>
2025-02-17 16:02:15 +0100

    ajout de la méthode getAddedProducts

--- cypress/e2e/AjoutPoduitsCard.cy.js
+++ cypress/e2e/AjoutPoduitsCard.cy.js
@@ -1,27 +1,29 @@
 /// <reference types="cypress" />
 
-import loginPage from "../pages/login.page";
-import productsPage from "../pages/products.page";
+import cartPage from "../pages/cart.page";
+import productPage from "../pages/product.page";
+
 
 Describe("Test affichage des produits ajoutés dans cart", ()=>{
     
     Context("Test affichage des produits ajoutés dans cart", () => {
         beforeEach(() =>{
+            cy.visit("https://www.saucedemo.com/");
             loginPage.login("standard_user", "secret_sauce");
         });
 
-        let addedProdcuts = ["Sauce Labs Backpack", "Sauce Labs Bike Light"];
-
         it("Ajouter des produits et vérifier s'ils sont ajoutés dans card", () => {
-            // ajouter plusieurs produits
-            addedProdcuts.forEach((produit) => {
-                productsPage.addProduits(produit);
-            });
+            // ajouter n produits
+            n = 2
+            productPage.elements.productTitle().then((products) => {
+                products.forEach((produit, id) => {
+                    if(id < n){
+                        productPage.addProduits(produit);
+                    }
+                });
+            })
             
-            productsPage.allerAuPanier();
-
-            
-
+            productPage.allerAuPanier();
 
         });
     });
--- cypress/pages/cart.page.js
+++ cypress/pages/cart.page.js
@@ -4,8 +4,8 @@ class cartPage {
         cartButton: () => cy.get('#shopping_cart_container'),
         badgeButton: () => cy.get('[data-test="shopping-cart-badge"]'),
         continueButton: () => cy.get('#continue-shopping'),
-        removeButton: () => cy.get('#remove-sauce-labs-backpack"')
-        
+        removeButton: () => cy.get('#remove-sauce-labs-backpack"'),
+        addedProducts: () => cy.get("[data-test='inventory-item-name']")
     }
  clickContinueButton(){
    this.elements.continueButton().click();
@@ -28,6 +28,9 @@ class cartPage {
     return this.elements.badgeButton();
 }
     
+  getAddedProducts(){
+    return this.elements.addedProducts();
+  }
 
   
 }
