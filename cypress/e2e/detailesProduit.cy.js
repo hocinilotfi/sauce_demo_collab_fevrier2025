5efb62e728bf34811664f86141e1541f405eae24
Tarek HALLALEL <TarekHALLALEL@gmail.com>
2025-02-18 10:43:30 +0100

    ajout du test detailles product

--- cypress/e2e/detailesProduit.cy.js
+++ cypress/e2e/detailesProduit.cy.js
@@ -1,20 +1,17 @@
 /// <reference types="cypress" />
 import loginPage from "../pages/login.page"
 import productsPage from "../pages/product.page"
-
 describe('detailles product', () => {
   context('success', () => {
     it('passes', () => {
       cy.visit('https://www.saucedemo.com/')
       loginPage.login("standard_user", "secret_sauce")
-      
       cy.get('[data-test="inventory-item-name"]').each(($itemname) => {
         const productName = $itemname.text().trim();
         console.log(productName)
         productsPage.cliquerSurProduit(productName)
         cy.url().should("include", "inventory-item");
-        cy.get(".inventory_details_name").should("contain",productName);
-        
+        cy.get(".inventory_details_name").should("contain", productName);
       })
     })
   })
